63555b7165470d2dbf4c5fcfa91599d2ebe287e6
Admin <tserenlkhagvaganbold@gmail.com>
2025-02-25 15:25:28 +0000

    daisyui intro and initial implement

--- front_end/src/components/navbar/Navbar.jsx
+++ front_end/src/components/navbar/Navbar.jsx
@@ -10,13 +10,15 @@ function Navbar () {
       <div className="flex-initial flex flex-col items-center justify-center h-full w-60 bg-green-400">
         <p className="text-lg sm:text-xl">Letter Exchange</p>
       </div>
-      <div className="flex-initial h-full w-40 bg-blue-600">
+      <div className="flex-initial h-full w-46 bg-blue-600">
         {loggedDetails.logged ? 
         <div className="h-full w-full bg-pink-400">
-
+          <button className="btn">Profile</button>
+          <button className="btn">Sign out</button>
         </div> :
-        <div className="h-full w-full bg-slate-400">
-          
+        <div className="flex flex-row justify-between items-center h-full w-full bg-slate-400">
+          <button className="btn">Log in</button>
+          <button className="btn">Register</button>
         </div>}
       </div>
     </div>
