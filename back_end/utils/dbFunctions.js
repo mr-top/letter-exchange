7916fa7288f8a082249363d40a1c21c2ec1523a3
Admin <tserenlkhagvaganbold@gmail.com>
2025-02-28 12:10:51 +0000

    hashes passwords, checks against hashed password. users from schema do not work anymore

--- back_end/utils/dbFunctions.js
+++ back_end/utils/dbFunctions.js
@@ -1,15 +1,28 @@
 const query = require('./query');
+const bcrypt = require('bcrypt');
+
+async function hashPassword (password) {
+  const hashedPassword = await bcrypt.hash(password, 2);
+  return hashedPassword;
+}
+
+async function checkPassword(password, hashedPassword) {
+  return await bcrypt.compare(password, hashedPassword);
+}
 
 async function login (username, password) {
-  const selectQuery = await query('SELECT * FROM users WHERE username = $1 AND password = $2', username, password);
+  const selectQuery = await query('SELECT * FROM users WHERE username = $1', username);
 
   if (selectQuery.success) {
     const result = selectQuery.result;
     
     if (result.rowCount > 0) {
 
-      const {id, username, email, description, country} = result.rows[0];
+      if (await checkPassword(password, result.rows[0].password)) {
+        const {id, username, email, description, country} = result.rows[0];
       return {success: true, msg: 'User successfully found', id, username, email, description, country}
+      };
+      return {success: false, msg: 'User successfully found but password not correct'}
     } else {
       return {success: false, msg: 'Could not find account with these records'}
     }
@@ -20,7 +33,8 @@ async function login (username, password) {
 }
 
 async function register (username, email, password, country) {
-  const insertQuery = await query('INSERT INTO users (username, email, password, country) VALUES ($1, $2, $3, $4)', username, email, password, country);
+  const hashedPassword = await hashPassword(password);
+  const insertQuery = await query('INSERT INTO users (username, email, password, country) VALUES ($1, $2, $3, $4)', username, email, hashedPassword, country);
 
   if (insertQuery.success) {
     const result = insertQuery.result;
