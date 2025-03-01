baf10920857abcf2b910500068cc3aca400139a4
Admin <tserenlkhagvaganbold@gmail.com>
2025-03-01 18:18:09 +0000

    letters component and gridbox

--- front_end/src/Home.jsx
+++ front_end/src/Home.jsx
@@ -2,17 +2,19 @@ import { useContext } from "react";
 
 import { LetterHistoryContext } from "./components/utils/LetterHistoryContext";
 
+import Letters from "./components/Letters";
+
 function Home () {
   const {setLookup, letters} = useContext(LetterHistoryContext);
 
   return (
     <div className="flex-1 flex min-h-120 bg-pink-400">
-      <div className="flex-1 bg-green-500">
-        
+      <div className="flex-1 max-h-120 place-items-center overflow-y-auto grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))] gap-2 p-2 bg-green-500">
+        <Letters letters={letters}/>
       </div>
       <div className="flex-initial w-50 bg-red-400">
         <button onClick={() => setLookup({method: 'open'})}>Open Letters</button>
-        <ul className="w-full h-full bg-yellow-400">
+        <ul className="w-full h-fit bg-yellow-400">
           
         </ul>
       </div>
---
+++ front_end/src/components/Letters.jsx
@@ -0,0 +1,11 @@
+function Letters ({letters}) {
+  return (
+    <>
+      {letters.map(letter => <div className="w-40 h-20 bg-red-400">
+
+      </div>)}
+    </>
+  )
+}
+
+export default Letters;
