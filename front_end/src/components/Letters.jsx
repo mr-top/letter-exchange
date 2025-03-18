49715e94357e1965cf882a4f8305d26a1f52fc8e
Admin <tserenlkhagvaganbold@gmail.com>
2025-03-18 16:45:44 +0000

    letter redesign

--- front_end/src/components/Letters.jsx
+++ front_end/src/components/Letters.jsx
@@ -21,36 +21,48 @@ function Letters({ setLookup, lookup, letters, loggedDetails }) {
     }
 
     document.getElementById('compose_modal').addEventListener('close', closeEvent)
-}
+  }
 
-return (
-  <>
-    {letters.map(letter => {
-      const letterDisabled = checkTime(letter.arrival_date);
-      letter.posted_date = new Date(letter.posted_date).toLocaleString();
-      return (
-        <button key={letter.id} className="btn w-32 h-20 btn-neutral" onClick={() => openLetter(letter)} disabled={letterDisabled}>
-          <p>Basic Letter</p>
-          <p>{letter.posted_date}</p>
-        </button>
-      )
-    })}
-    <dialog id="letter-modal" className="modal">
-      <div className="modal-box">
-        <h2 className="text-md">To: {currentLetter.recipient_username || 'Everyone'}</h2>
-        <h2 className="text-md">From: {currentLetter.sender_username}</h2>
-        <h2 className="text-md">Posted date: {currentLetter.posted_date}</h2>
-        <p>{currentLetter.content}</p>
-        <div className="modal-action">
-          {currentLetter.sender_id === loggedDetails.id || <button className="btn btn-accent" onClick={() => replyLetter(currentLetter.sender_id)}>Reply</button>}
-          <form method="dialog">
-            <button className="btn">Close</button>
-          </form>
+  return (
+    <>
+      {letters.map(letter => {
+        const letterDisabled = checkTime(letter.arrival_date);
+        letter.posted_date = new Date(letter.posted_date).toLocaleString();
+        return (
+          <button key={letter.id} className="btn w-40 h-24 flex flex-col px-2 py-1 bg-yellow-50" onClick={() => openLetter(letter)} disabled={letterDisabled}>
+            <div className="flex-5/12 flex justify-between items-center w-full">
+              <div className="flex-initial border-1 border-black rounded-2xl w-12 h-8">
+                <p className="text-[9px]">XX</p>
+                <p className="text-[7px]">Post office</p>
+              </div>
+              <div className="flex-initial border-1 border-black bg-yellow-200 w-6 h-8">
+
+              </div>
+            </div>
+            <div className="flex-7/12 flex justify-end items-center w-full">
+              <div className="w-20 h-10">
+                <p className="text-[10px]">{letter.recipient_username} {letter.recipient_id}</p>
+              </div>
+            </div>
+          </button>
+        )
+      })}
+      <dialog id="letter-modal" className="modal">
+        <div className="modal-box">
+          <h2 className="text-md">To: {currentLetter.recipient_username || 'Everyone'}</h2>
+          <h2 className="text-md">From: {currentLetter.sender_username}</h2>
+          <h2 className="text-md">Posted date: {currentLetter.posted_date}</h2>
+          <p>{currentLetter.content}</p>
+          <div className="modal-action">
+            {currentLetter.sender_id === loggedDetails.id || <button className="btn btn-accent" onClick={() => replyLetter(currentLetter.sender_id)}>Reply</button>}
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
         </div>
-      </div>
-    </dialog>
-  </>
-)
+      </dialog>
+    </>
+  )
 }
 
 export default Letters;
