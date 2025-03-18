56a304515738c634064db29838cd3389f5b9629a
Admin <tserenlkhagvaganbold@gmail.com>
2025-03-18 17:49:58 +0000

    login and register page redesign

--- front_end/src/components/Login.jsx
+++ front_end/src/components/Login.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from "react";
+import { Link } from 'react-router-dom';
 
 import { UserContext } from "./utils/UserContext";
 
@@ -18,20 +19,20 @@ function Login() {
   }
 
   return (
-    <form className="flex flex-col justify-center py-2 px-10 w-70 h-80 bg-pink-400" onSubmit={login}>
+    <form className="flex flex-col justify-center py-2 px-10 w-70 h-80 border-1 border-base-content" onSubmit={login}>
       <div className="flex-initial">
-        <label>Username:</label>
+        <label className="opacity-75 text-sm">Username:</label>
         <input type="text" value={username} onChange={e => setUsername(e.currentTarget.value)} className="w-full h-8 px-2" />
       </div>
       <div className="flex-initial">
-        <label>Password:</label>
+        <label className="opacity-75 text-sm">Password:</label>
         <input type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} className="w-full h-8 px-2" />
       </div>
       <div className="flex justify-between p-2">
-        <button className="flex-initial btn w-20" type="submit" disabled={loading}>Log in</button>
+        <button className="flex-initial btn btn-accent w-20" type="submit" disabled={loading}>Log in</button>
         <div className="flex-initial w-20">
-          <p className="text-xs">Don't have an account?</p>
-          <p className="text-sm">Create one</p>
+          <p className="opacity-75 text-xs">Don't have an account?</p>
+          <Link to='/register'><p className="text-sm font-medium">Create one</p></Link>
         </div>
       </div>
       {status && (status.success ? 'It worked' : 'It did not work')}
--- front_end/src/components/Register.jsx
+++ front_end/src/components/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 import axios from "axios";
 import getGeo from "../utils/getGeo";
@@ -27,32 +28,32 @@ function Register() {
   }
 
   return (
-    <form className="flex flex-col justify-center py-2 px-10 w-70 min-h-100 max-h-120 bg-pink-400" onSubmit={register}>
+    <form className="flex flex-col justify-center py-2 px-10 w-70 min-h-100 max-h-120 border-1 border-base-content" onSubmit={register}>
       <div className="flex-initial">
-        <label>Username:</label>
+        <label className="opacity-75 text-sm">Username:</label>
         <input type="text" value={username} onChange={e => setUsername(e.currentTarget.value)} className={`w-full h-8 px-2 ${tried && username && !inputVerify.checkUsername(username) && 'border-red-400'}`} />
         <p className="text-xs">{tried && username && !inputVerify.checkUsername(username) && 'Username must be made of 6-16 letters'}</p>
       </div>
       <div className="flex-initial">
-        <label>Email:</label>
+        <label className="opacity-75 text-sm">Email:</label>
         <input type="text" value={email} onChange={e => setEmail(e.currentTarget.value)} className={`w-full h-8 px-2 ${tried && email && !inputVerify.checkEmail(email) && 'border-red-400'}`} />
         <p className="text-xs">{tried && email && !inputVerify.checkEmail(email) && 'Must be valid email'}</p>
       </div>
       <div className="flex-initial">
-        <label>Password:</label>
+        <label className="opacity-75 text-sm">Password:</label>
         <input type="password" value={password} onChange={e => setPassword(e.currentTarget.value)} className={`w-full h-8 px-2 ${tried && password && !inputVerify.checkPassword(password) && 'border-red-400'}`} />
         <p className="text-xs">{tried && password && !inputVerify.checkPassword(password) && 'Password must have minimum of 8 characters and once uppercase letter'}</p>
       </div>
       <div className="flex-initial">
-        <label>Confirm password:</label>
+        <label className="opacity-75 text-sm">Confirm password:</label>
         <input type="password" value={confirmPassword} onChange={e => setConfirmPassword(e.currentTarget.value)} className={`w-full h-8 px-2 ${confirmPassword && !inputVerify.checkDuplicate(password, confirmPassword) && 'border-red-400'}`} />
         <p className="text-xs">{confirmPassword && !inputVerify.checkDuplicate(password, confirmPassword) && 'Passwords must match'}</p>
       </div>
       <div className="flex justify-between p-2">
-        <button className="flex-initial btn w-22" type="submit" disabled={loading}>Sign Up</button>
-        <div className="flex-initial w-20">
-          <p className="text-xs">Already have an account</p>
-          <p className="text-sm">Log in instead</p>
+        <button className="flex-initial btn btn-accent w-22" type="submit" disabled={loading}>Sign Up</button>
+        <div className="flex-initial w-21">
+          <p className="opacity-75 text-xs">Already have an account?</p>
+          <Link to='/login'><p className="text-xs font-medium">Log in instead</p></Link>
         </div>
       </div>
     </form>
